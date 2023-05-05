3ebbbd35cd67d2002ec0865680faf100002bba63
kvizhard <111001715+kvizhard777@users.noreply.github.com>
2023-05-05 20:32:21 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const botToken = '6268753758:AAFV9rRiZsgNItf1Ydd09dst-bOabKytHlQ';
+const chatId = '2091107961';
+
+function sendTelegram() {
+    const form = document.getElementById('main-form');
+    let message = '❗<b>Новые данные</b>\n\n';
+
+    form.querySelectorAll('textarea').forEach((textarea, index) => {
+        const answer = textarea.value.trim();
+        const question = questions[index];
+    
+        message += `<b>${question}</b>\n<i>- ${answer}</i>\n\n`;
+    });
+
+    const encodedMessage = encodeURIComponent(message);
+
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${encodedMessage}`);
+};
